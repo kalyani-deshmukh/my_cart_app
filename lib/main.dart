4a20b564774c989081d18e8c69783580c490eb4a
kalyani deshmukh <kalyanideshmukh@gmail.com>
2021-06-30 11:27:42 +0530

    day 3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:my_cart_app/home_page.dart';
+import 'package:my_cart_app/pages/home_page.dart';
+import 'package:my_cart_app/pages/login_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,7 +10,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Homepage(),
+      //home: Homepage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      initialRoute: "/home",
+      routes: {
+        "/" : (context) => LoginPage(),
+        "/home" : (context) => Homepage(),
+        "/login": (context) => LoginPage(),
+      },
     );
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -6,7 +6,9 @@ class Homepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Text("Catalog App"),
+      ),
       body: Center(
         child: Container(
           child: Text("Welcome to the Future kuDo!"),
