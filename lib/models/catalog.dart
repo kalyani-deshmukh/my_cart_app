8fdba65c9ecd3b2aad0bc4e105bacb44eebd1952
kalyani deshmukh <kalyanideshmukh@gmail.com>
2021-07-03 11:30:33 +0530

    Listview builder, card concept

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,5 +1,23 @@
+
+class CatalogModel{
+
+static final items = [
+  Item(
+  id: 1,
+  name:"Athena",
+  desc: "Charming Fuchsia Pink Power Shoulders Tulip Hem Wrap Dress",
+  price: 979,
+  color:"#33505a",
+  image: "https://assets.myntassets.com/dpr_1.5,q_60,w_400,c_limit,fl_progressive/assets/images/13719160/2021/3/5/b04a5dc0-9beb-4490-ae2c-f0f9654ded1c1614925067822-Athena-Fuchsia-Pink-dress-with-Power-shoulderS--Tulip-Bottom-1.jpg",
+  ),
+];
+}
+
+
+
+
 class Item {
-  final String id;
+  final int id;
   final String name;
   final String desc;
   final num price;
@@ -9,13 +27,4 @@ class Item {
   Item({required this.id, required this.name, required this.desc, required this.price, required this.color, required this.image});
 }
 
-final products = [
-  Item(
-  id: "item001",
-  name:"Athena",
-  desc: "Charming Fuchsia Pink Power Shoulders Tulip Hem Wrap Dress",
-  price: 979,
-  color:"#33505a",
-  image: "https://assets.myntassets.com/dpr_1.5,q_60,w_400,c_limit,fl_progressive/assets/images/13719160/2021/3/5/b04a5dc0-9beb-4490-ae2c-f0f9654ded1c1614925067822-Athena-Fuchsia-Pink-dress-with-Power-shoulderS--Tulip-Bottom-1.jpg",
-  ),
-];
+
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,19 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:my_cart_app/models/catalog.dart';
 import 'package:my_cart_app/widgets/drawer.dart';
+import 'package:my_cart_app/widgets/item_widget.dart';
 
 class Homepage extends StatelessWidget {
   const Homepage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: Text("Catalog App"),
-        ),
-      body: Center(
-        child: Container(
-          child: Text("Welcome to the Future kuDo!"),
-        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+            itemCount: dummyList.length,
+            itemBuilder: (context, index) {
+              return ItemWidget(
+                item: dummyList[index],
+              );
+            }),
       ),
       drawer: MyDrawer(),
     );
